e6127b820249952647ffa5df730c4badab80cf62
Dreptus <dreptus@gmai.com>
2022-07-27 15:34:59 +0200

    Working Home Page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Home from "./components/pages/Home/Home";
 import NotFound from "./components/pages/PageNotFound/PageNotFound";
-import Posts from "./components/features/Post/Posts";
+import Posts from "./components/pages/Post/Posts";
 import About from "./components/pages/About/About";
 import PostAdd from './components/pages/PostAdd/PostsAdd'
+import PostEdit from './components/pages/PostEdit/PostEdit'
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
 
@@ -14,11 +15,11 @@ const App = () => {
       <Container>
         <Header/>
         <Routes>
-          <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
           <Route path="/post/:id" element={<Posts />} />
           <Route path="/post/add" element={<PostAdd />} />
-          <Route path="/post/edit" element={<PostAdd />} />
+          <Route path="/post/edit/:postId" element={<PostEdit />} />
+          <Route path="/about" element={<About />} />
           <Route path='*' element={<NotFound/>}/>
         </Routes>
         <Footer/>
